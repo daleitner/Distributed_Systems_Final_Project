83c7265e1af8a2128b6fa3bf07ff4cd7375db96e
Julian Janda <jjanda@edu.uni.at>
2015-06-28 17:31:39 +0200

    Initial Java Project setup. I set up a project and started implementing the map reduce task.
    Hint: Please import this project WITHOUT selcting "Copy this project into workspace"

---
+++ Java/ImageSearch_Hadoop/src/mapReduce/ImageSearch.java
@@ -0,0 +1,39 @@
+package mapReduce;
+
+
+/**
+ * TODO Re-organise setup so these imports work.
+ */
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class ImageSearch {
+
+	public static void main(String[] args) throws Exception {
+		if (args.length != 2) {
+			System.err.println("Usage: ImageSearch <input path> <output path>");
+			System.exit(-1);
+		}
+		
+		Job job = new Job();
+		job.setJarByClass(ImageSearch.class);
+		job.setJobName("ImageSearch");
+		
+		FileInputFormat.addInputPath(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+		
+		job.setMapperClass(ImageSearchMapper.class);
+		job.setReducerClass(ImageSearchReducer.class);
+		
+		//Those two have yet to be adjusted for our needs
+//		job.setOutputKeyClass(Text.class);
+//		job.setOutputValueClass(IntWritable.class);
+		
+		System.exit(job.waitForCompletion(true)?0:1);
+	}
+	
+}
---
+++ Java/ImageSearch_Hadoop/src/mapReduce/ImageSearchReducer.java
@@ -0,0 +1,5 @@
+package mapReduce;
+
+public class ImageSearchReducer {
+
+}
